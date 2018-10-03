b6430bed84958e37deb756df77d14bfddbefc370
Tech-A36 <tech-a36@tech-a36s-air.home>
2018-10-03 21:47:48 +0100

    more validations added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,9 @@
 class Blog < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :user_id, :presence => true
   validates :title, :presence => true
   validates :body, :presence => true
-  # validates :body, length: {maximum: 240}
   validates :image, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :first_name, :presence => true
+  validates :first_name, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/, :message => 'Please enter letters only' }
+
   validates :last_name, :presence => true
+  validates :last_name, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/, :message => 'Please enter letters only' }
+
+  validates :confirm, :acceptance => true
+
 
   def full_name
     "#{first_name} #{last_name}"
