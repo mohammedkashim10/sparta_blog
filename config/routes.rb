935f9e55d86e6dca985d8643f5f1a04ce809c503
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-10-03 11:39:02 +0100

    blogs controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  resources :blogs
 end
