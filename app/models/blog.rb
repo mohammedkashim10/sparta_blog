1bd53efb7286803138b3f5380e9fdc6a8881bfaa
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-10-03 18:14:43 +0100

    more validations added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,5 +2,6 @@ class Blog < ApplicationRecord
   belongs_to :user
   validates :title, :presence => true
   validates :body, :presence => true
+  validates :body, length: {maximum: 240}
   validates :image, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :blogs
   validates :first_name, :presence => true
+  validates :first_name, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ }
   validates :last_name, :presence => true
+  validates :last_name, :format => { :with => /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ }
 end
