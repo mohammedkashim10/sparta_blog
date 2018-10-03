f615b9c9e0b06b16582c1d4712efd1baa3707c82
Tech-A36 <tech-a36@Tech-A36s-MacBook-Air.local>
2018-10-03 14:01:42 +0100

    added comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'static#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
 end
